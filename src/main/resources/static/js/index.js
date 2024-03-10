3f01793c5bac1e0c88703365f756d41f03fb2dfc
Firewolf <dhanarajjangi18@gmail.com>
2024-03-10 23:55:49 +0530

    employee update code added

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -30,24 +30,31 @@ function bindHomePageEvents(){
 			let obj = data[i];
 			let index = i+1;
 			let tr = '<tr><td scope="row" style="display:none">'+obj.id+'</td><td>'+index+'</td><td>'+obj.firstName+'</td><td>'+obj.lastName+'</td><td>'+obj.designation+'</td><td>'+obj.password+'</td><td>'+obj.salary+'</td><td>'+obj.department+'</td>'
-			+'<td><button type="button" class="btn btn-primary emp-edit m-2">Edit</button><button type="button" class="btn btn-danger emp-delete m-2">Delete</button></td></tr>'
+			+'<td><button type="button" class="btn btn-primary emp-update m-2">Update</button><button type="button" class="btn btn-danger emp-delete m-2">Delete</button></td></tr>'
 			tbody.append(tr);
 		}
     });
     
-    $('.employeeTbody').off('click').on('click','.emp-edit',function(){
+    $('.Employee-table').off('click').on('click','.emp-update',function(){
 	     $('.addEmployeePage').hide();
 		 $('.homePage').hide();
 		 $('.editEmployeePage').show();
 		 let myid = $(this).parent().parent().children().eq(0).html(); 
 		 
 		$.get('/get/'+myid, function (data) {
-			 console.log(data);
+			console.log(data);
+			$('#employee-id').html(data.id);
+			$('#emp-edit-FirstName').val(data.firstName);
+			$('#emp-edit-LastName').val(data.lastName);
+			$('#emp-edit-Designation').val(data.designation);
+			$('#emp-edit-Salary').val(data.salary);
+			$('#emp-edit-Department').val(data.department);
+			$('#emp-edit-password').val(data.password);
 		});
 		 
 	});
 	
-	$('.employeeTbody').off('click').on('click','.emp-delete',function(){
+	$('#employee-table-edit').off('click').on('click','.emp-delete',function(){
 		 $(this).parent().parent().remove();
 		 let id = $(this).parent().parent().children().eq(0).html();
 	     $.ajax({
@@ -78,6 +85,40 @@ function bindHomePageEvents(){
 		      $('.nav-link').eq(0).trigger('click');
 		})    
 	});
+	
+	$('#employee-update').off('click').on('click',function(){
+		
+		let firstName = $('#emp-edit-FirstName').val();
+		let lastName = $('#emp-edit-LastName').val();
+		let designation = $('#emp-edit-Designation').val();
+		let salary = $('#emp-edit-Salary').val();
+		let department = $('#emp-edit-Department').val();
+		let password = $('#emp-edit-password').val();
+		
+		let myid = $('#employee-id').html();
+		let jsondata = {firstName : firstName, lastName : lastName, designation : designation, salary : salary, department : department, password : password};
+		
+		$.ajax({
+		    url: "/update/"+myid,
+		    type: "PUT",
+		    data: jsondata,
+		    success: function(data) {
+		        console.log(data);
+		        $('.nav-link').eq(0).trigger('click');
+		    },
+		    error: function() {
+		    }
+		});
+		
+		/*$.post('/update/'+myid,   // url
+		{ firstName : firstName, lastName : lastName, designation : designation, salary : salary, department : department, password : password  }, // data to be submit
+		   function(data, status, jqXHR) {// success callback
+		      console.log(data);
+		      $('.nav-link').eq(0).trigger('click');
+		}) */   
+	});
+	
+	
 		
 }
 
